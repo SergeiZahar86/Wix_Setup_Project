fd4e220fb38d18b84b62c2f91d2eaa1fb59c47ec
Сергей Захаров <sergei86zahar@mail.ru>
2021-03-10 16:32:37 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приложение из установщика WIX.");
+            Console.ReadKey();
+        }
+    }
+}
